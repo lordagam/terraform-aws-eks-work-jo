949b43dab36cf360007f4443db94f7a3eedefe58
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-02-09 09:42:04 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,11 +11,11 @@ terraform {
     }
   }
 }
-  #required_providers {
-    #aws = {
-      #source  = "hashicorp/aws"
-      #version = "~> 5.7.0"
-    #}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
 
     random = {
       source  = "hashicorp/random"
